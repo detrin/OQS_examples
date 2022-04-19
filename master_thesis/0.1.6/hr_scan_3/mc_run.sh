a4624f22affb0c4790c1c01491fa4564859f4ff1
Daniel Herman <daniel.herman@protonmail.com>
2022-04-19 16:36:35 +0200

    updated submit script

--- master_thesis/0.1.6/hr_scan_3/mc_run.sh
+++ master_thesis/0.1.6/hr_scan_3/mc_run.sh
@@ -3,25 +3,30 @@
 #PBS -l walltime=24:00:00
 #PBS -m ae
 
-DATADIR=/storage/praha1/home/hermanda/OQS_examples/master_thesis/0.1.6/hr_scan_3
+cat $PBS_NODEFILE
+hostname
+cd ${PBS_O_WORKDIR}
+echo $(pwd)
+
+# DATADIR=/storage/praha1/home/hermanda/OQS_examples/master_thesis/0.1.6/hr_scan_3
 
 module add julia/julia-1.7.0-gcc-8.3.0-rdhfzdi
 # export JULIA_PKGDIR=$HOME/.julia
 # export JULIA_DEPOT_PATH=/storage/brno2/home/hermanda/.julia
 echo $HOSTNAME
 
-test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
-echo $(pwd)
-cd $SCRATCHDIR
-echo $(pwd)
+# test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
+# echo $(pwd)
+# cd $SCRATCHDIR
+# echo $(pwd)
 
-cp $DATADIR/main.jl main.jl
-cp $DATADIR/update_julia.sh update_julia.sh
-chmod +x update_julia.sh
+# cp $DATADIR/main.jl main.jl
+# cp $DATADIR/update_julia.sh update_julia.sh
+# chmod +x update_julia.sh
 source update_julia.sh
 
 echo $(ls)
 julia main.jl -n $n
-cp data/*.h5 $DATADIR/data/
+# cp data/*.h5 $DATADIR/data/
 
 clean_scratch
