91a4413e98e82501b802ad9a9827f5f6e10084c5
Daniel Herman <daniel.herman@protonmail.com>
2022-04-24 10:15:21 +0200

    cleaning

--- master_thesis/0.1.6/hr_scan_4_v2/mc_run.sh
+++ master_thesis/0.1.6/hr_scan_4_v2/mc_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=4gb:scratch_local=10gb:cluster=elmo3
-#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=20gb:scratch_local=10gb
+#PBS -l walltime=196:00:00
 #PBS -m ae
 
 cat $PBS_NODEFILE
