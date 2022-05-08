e98c89d83efc881dad9140d2ab3bdac83993f266
Daniel Herman <daniel.herman@protonmail.com>
2022-05-08 13:11:35 +0200

    sub script setup

--- master_thesis/0.1.7/hr_scan_1_100_005/mc_run.sh
+++ master_thesis/0.1.7/hr_scan_1_100_005/mc_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=2:mem=4gb:scratch_local=10gb:cluster=elmo3
+#PBS -l select=1:ncpus=1:mem=4gb:scratch_local=10gb:cluster=elmo3
 #PBS -l walltime=24:00:00
 #PBS -m ae
 
@@ -28,7 +28,7 @@ source update_julia.sh
 echo $(ls)
 ulimit -s unlimited
 mkdir -p data
-julia -p2 main.jl -n $n
+julia main.jl -n $n
 # cp data/*.h5 $DATADIR/data/
 
 clean_scratch
