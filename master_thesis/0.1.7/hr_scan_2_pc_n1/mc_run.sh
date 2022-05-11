51a1db3e29cbec47b6eef18bc76c5b81a67ecd04
Daniel Herman <daniel.herman@protonmail.com>
2022-05-11 20:05:58 +0200

    corrected sub script

--- master_thesis/0.1.7/hr_scan_2_pc_n1/mc_run.sh
+++ master_thesis/0.1.7/hr_scan_2_pc_n1/mc_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=4gb:scratch_local=10gb:cluster=elmo3
+#PBS -l select=1:ncpus=1:mem=4gb:scratch_local=10gb:cluster=kirke
 #PBS -l walltime=48:00:00
 #PBS -m ae
 
